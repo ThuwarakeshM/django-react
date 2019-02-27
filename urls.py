3b84c9bb787c180d0c17c17b7591f25ac6b3540d
Thuwarakesh <thuwarakesh@gmail.com>
2019-02-27 08:53:02 +0530

    restructure

--- urls.py
+++ urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='frontend/index.html'), name='home'),
+    path('', TemplateView.as_view(template_name='django-react/index.html'), name='home'),
 ]
